0c08a151e4c1ffef5a3428d89cd7c6673c32c334
Louis Reed <mail@louisreed.co.uk>
2023-01-07 21:45:08 +0000

    Rewrote plugin code

--- wordpress-custom-favicon.php
+++ wordpress-custom-favicon.php
@@ -8,70 +8,77 @@ Author: Louis Reed
 Author URI: https://louisreed.co.uk
 */
 
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-  exit;
+function custom_favicon_menu() {
+    add_options_page( 'Custom Favicon', 'Custom Favicon', 'manage_options', 'custom-favicon', 'custom_favicon_options' );
 }
+add_action( 'admin_menu', 'custom_favicon_menu' );
 
-// Register the plugin settings
-function custom_favicon_register_settings() {
-    add_option( 'custom_favicon', '', '', 'yes' );
-    register_setting( 'custom_favicon_group', 'custom_favicon', 'custom_favicon_callback' );
-}
-add_action( 'admin_init', 'custom_favicon_register_settings' );
+function custom_favicon_options() {
+    if ( ! current_user_can( 'manage_options' ) ) {
+        wp_die( 'You do not have sufficient permissions to access this page.' );
+    }
 
-// Add the plugin settings page
-function custom_favicon_create_menu() {
-    add_options_page( 'Custom Favicon Settings', 'Custom Favicon', 'manage_options', 'custom-favicon', 'custom_favicon_settings_page' );
-}
-add_action( 'admin_menu', 'custom_favicon_create_menu' );
+    // Save the form data if the Save Changes button is clicked
+    if ( isset( $_POST['custom_favicon_form_submitted'] ) ) {
+        check_admin_referer( 'custom_favicon_form_action', 'custom_favicon_form_nonce_field' );
 
-// Display the plugin settings page
-function custom_favicon_settings_page() {
-    ?>
-    <div class="wrap">
-        <h1>Custom Favicon Settings</h1>
-        <form method="post" action="options.php">
-            <?php settings_fields( 'custom_favicon_group' ); ?>
-            <?php do_settings_sections( 'custom_favicon_group' ); ?>
-            <table class="form-table">
-                <tr valign="top">
-                    <th scope="row">Favicon</th>
-                    <td>
-                        <input type="text" name="custom_favicon" value="<?php echo esc_attr( get_option('custom_favicon') ); ?>" />
-                        <input id="upload_image_button" type="button" class="button" value="Upload Image" data-choose="Choose an Image" data-update="Select" />
-                        <br />
-                        <p class="description">Enter the URL for the favicon image or use the upload button to select an image from the media library.</p>
+        $custom_favicon_url = sanitize_text_field( $_POST['custom_favicon'] );
+        update_option( 'custom_favicon_url', $custom_favicon_url );
+        ?>
+        <div class="updated"><p><strong><?php _e( 'Changes saved.', 'custom-favicon' ); ?></strong></p></div>
+        <?php
+    }
+
+// Display the form
+?>
+<div class="wrap">
+    <h2><?php _e( 'Custom Favicon', 'custom-favicon' ); ?></h2>
+    <form name="custom_favicon_form" method="post" action="">
+        <?php wp_nonce_field( 'custom_favicon_form_action', 'custom_favicon_form_nonce_field' ); ?>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row"><label for="custom_favicon"><?php _e( 'Favicon URL', 'custom-favicon' ); ?></label></th>
+                <td>
+                    <input type="text" name="custom_favicon" id="custom_favicon" value="<?php echo esc_url( get_option( 'custom_favicon_url' ) ); ?>" size="60" />
+                    <input id="upload_image_button" type="button" class="button" value="Upload Image" />
+                    <p class="description"><?php _e( 'Enter the URL for the favicon image, or use the upload button to select an image from the media library.', 'custom-favicon' ); ?></p>
+                    <?php
+                        // Display the image thumbnail if an image URL has been entered
+                        $custom_favicon_url = esc_url( get_option( 'custom_favicon_url' ) );
+                        if ( ! empty( $custom_favicon_url ) ) {
+                            echo '<br /><img src="' . $custom_favicon_url . '" style="max-width:100px;height:auto;margin:10px 0;" />';
+                        }
+                        ?>
                     </td>
                 </tr>
             </table>
-            <?php submit_button(); ?>
+            <p class="submit">
+                <input type="submit" name="custom_favicon_form_submitted" class="button-primary" value="<?php _e( 'Save Changes', 'custom-favicon' ); ?>" />
+            </p>
         </form>
     </div>
     <?php
 }
 
-// Sanitize and validate the input
-function custom_favicon_callback( $input ) {
-    $input = esc_url( $input );
-    return $input;
-}
-
-// Add the favicon to the front-end of the site
-function custom_favicon_wp_head() {
-    $favicon_url = get_option( 'custom_favicon' );
-    if ( !empty( $favicon_url ) ) {
-        echo '<link rel="shortcut icon" href="' . esc_url( $favicon_url ) . '" />';
-    }
-}
-add_action( 'wp_head', 'custom_favicon_wp_head' );
-
 function custom_favicon_enqueue_scripts() {
-  // Enqueue the WordPress media library scripts
-  wp_enqueue_media();
+    // Enqueue the WordPress media library scripts
+    wp_enqueue_media();
 
-  // Register and enqueue the custom script
-  wp_register_script( 'custom-favicon', plugin_dir_url( __FILE__ ) . 'custom-favicon.js', array( 'wp-media-uploader' ), '1.0', true );
-  wp_enqueue_script( 'custom-favicon' );
+    // Register and enqueue the custom script
+    wp_register_script( 'custom-favicon', plugin_dir_url( __FILE__ ) . 'custom-favicon.js', array( 'wp-media-uploader' ), '1.0', true );
+    wp_enqueue_script( 'custom-favicon' );
 }
 add_action( 'admin_enqueue_scripts', 'custom_favicon_enqueue_scripts' );
+
+function custom_favicon() {
+    // Get the custom favicon URL
+    $custom_favicon_url = esc_url( get_option( 'custom_favicon_url' ) );
+
+    // If a custom favicon URL has been entered, use it
+    if ( ! empty( $custom_favicon_url ) ) {
+        echo '<link rel="shortcut icon" href="' . $custom_favicon_url . '" />';
+    }
+}
+add_action( 'wp_head', 'custom_favicon' );
+
+
